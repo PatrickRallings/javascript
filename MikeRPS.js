f4de19b44b4338e71bae60e90e55b5e08ebc70b4
Patrick Rallings <patrickrallings@Patricks-iMac.local>
2022-03-25 11:08:15 -0400

    Per update

--- MikeRPS.js
+++ MikeRPS.js
@@ -1,4 +1,10 @@
 const hands = ['rock' , 'paper', 'scissors'];
+
+function getHand () {
+    n = parseInt(Math.random()*10)%3;
+    return hands[n];
+    }
+console.log(getHand())
 Player1 = {
     name: "Ya boi",
     hand: getHand(),
@@ -7,58 +13,25 @@ Player2 = {
     name: "Bill Nye",
     hand: getHand(),
 };
-function getHand () {
-    n = parseInt(Math.random()*10)%3;
-    if (n == 0) {
-        return hands[0];
-    } else if (n == 1) {
-        return hands[1];
-    } else if (n == 2) {
-        return hands[2];
+function playRound(player1, player2) {
+    let hand1 = player1.hand
+    let hand2 = player2.hand
+    
+    if ((hand1 === 'rock' && hand2 === 'scissors')||(hand1 === 'paper' && hand2 === 'rock')||(hand1 === 'scissors' && hand2 === 'paper')) {
+        console.log(player1.name+' played '+hand1+' and '+player2.name+' played '+hand2+'. '+player1.name+' wins!')
+        return player1;
+    }
+    else if ((hand2 === 'rock' && hand1 === 'scissors')||(hand2 === 'paper' && hand1 === 'rock')||(hand2 === 'scissors' && hand1 === 'paper')) {
+        console.log(player1.name+' played '+hand1+' and '+player2.name+' played '+hand2+'. '+player2.name+' wins!')
+        return player2;
+    }
+    else {
+        console.log(player1.name+' and '+player2.name+' tied.')
+        return null;
     }
 }
-function playRound() {
-    console.log(Player1.name, 'plays', Player1.hand);
-    console.log(Player2.name, 'plays', Player2.hand);
-    if (Player1.hand === Player2.hand) {
-        console.log('It is a tie!');
-    } else if (Player1.hand === hands[0]) {
-        if (Player2.hand === hands[2]) {
-            console.log(`${Player1.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`
-        } else {
-            console.log(`${Player2.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        }
-    } else if (Player1.hand === hands[0]) {
-        if (Player2.hand === hands[1]) {
-            console.log(`${Player2.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        } else {
-            console.log(`${Player1.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        }
-    } else if (Player1.hand === hands[1]) {
-        if (Player2.hand === hands[2]) {
-            console.log(`${Player1.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        } else {
-            console.log(`${Player2.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        }
-    } else if (Player1.hand === hands[2]) {
-        if (Player2.hand === hands[0]) {
-            console.log(`${Player2.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        } else {
-            console.log(`${Player1.name} wins!`);
-            return `${Player1Score} - ${Player2Score}`;
-        }
-    }
-}
-function PlayerWinner (player, playUntil) {
-    return player.score >= playUntil;
-}
+let player1Wins = 0;
+let player2Wins = 0;
 function playGame (Player1, Player2, playUntil) {
     let Player1Score = 0;
     let Player2Score = 0;
--- javascriptDay4/this.js
+++ javascriptDay4/this.js
@@ -18,6 +18,32 @@ const photoSlideShow = {
     }
   },
   getCurrentPhoto: function () {
-    console.log(this.photoList[this.currentPhotoIndex]);
+    return this.photoList[this.currentPhotoIndex];
   },
 };
+
+js '''
+const photoSlideShow = {
+  photoList: ["photo1", "photo2", "photo3"],
+  currentPhotoIndex: 0,
+  nextPhoto: function () {
+    if (this.currentPhotoIndex < this.photoList.length-1) {
+      this.currentPhotoIndex++;
+      console.log(this.photoList[this.currentPhotoIndex]);
+    } else {
+      console.log("End of slideshow.");
+    }
+  },
+  prevPhoto: function () {
+    if (this.currentPhotoIndex > 0) {
+      this.currentPhotoIndex--;
+      console.log(this.photoList[this.currentPhotoIndex]);
+    } else {
+      console.log("Beginning of slideshow.");
+    }
+  },
+  getCurrentPhoto: function () {
+    return this.photoList[this.currentPhotoIndex];
+  },
+};
+'''
