00f18e0691b7cc71e57a7bbb2ef6babffed5fa05
Patrick Rallings <patrickrallings@Patricks-MBP.localdomain>
2022-03-28 16:09:53 -0400

    Added class assignment

---
+++ javascriptWeek2/classAssignment.js
@@ -0,0 +1,41 @@
+class Job {
+    constructor(name, industry, requiresDegree, field, compensation) {
+        this.name = name
+        this.industry = industry
+        this.requiresDegree = requiresDegree
+        this.field = field
+        this.compensation = compensation
+    }
+    getName() {
+        return 'The name of the job is '+this.name;
+    }
+    getFieldandIndustry() {
+        return this.name+' is in the field of '+this.field+' and in the industry of '+this.industry+'.'
+    }
+    getCompensation() {
+        return 'The compensation for the job of '+this.name+' is '+this.compensation+'.'
+    }
+}
+ class Opportunity extends Job {
+     constructor(name, industry, requiresDegree, field, compensation, growingField) {
+        super (name, industry, requiresDegree, field, compensation)
+        this.growingField = growingField
+        }
+ }
+ const forester = new Job('Forester', 'Agriculture', true, 'Forestation', 62000)
+ const architect = new Job('Architect', 'Construction', true, 'Architecture', 81000)
+ const editor = new Job('Editor', 'Arts', false, 'Audio/Video Technology', 37000)
+
+ console.log(forester.getName())
+ console.log(forester.getCompensation())
+
+ console.log(architect.getCompensation())
+ console.log(architect.getFieldandIndustry())
+
+ console.log(editor.getName())
+ console.log(editor.getCompensation())
+
+ const softwareDeveloper = new Opportunity('Software Developer', 'Technology', false, 'Technology', 110000, true)
+
+ console.log(softwareDeveloper.getCompensation())
+ console.log(softwareDeveloper.getFieldandIndustry())
---
+++ test.js
@@ -0,0 +1,9 @@
+let arr = ['dog','cat','fish'];
+
+for (let i=0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// 'dog'
+// 'cat'
+// 'fish'
