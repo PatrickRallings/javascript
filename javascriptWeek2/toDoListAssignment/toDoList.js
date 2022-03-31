8b2c7db4ff2bb9285d51b4d4966458ba0f44b979
Patrick Rallings <patrickrallings@Patricks-iMac.local>
2022-03-30 21:53:55 -0400

    Worked on javascript for to-do list

--- javascriptWeek2/toDoListAssignment/toDoList.js
+++ javascriptWeek2/toDoListAssignment/toDoList.js
@@ -1,10 +1,20 @@
-document.getElementById("addButton").onclick = function() {
-    //First things first, we need our text:
-    var text = document.getElementById('exampleFormControlInput1').Value; //.value gets input values
+const listofToDoItems = document.getElementById('listActual')
+const listInput = document.querySelector('input')
+const inputButton = document.getElementById('addButton')
+let newItem = document.createElement('li')
 
-    //Now construct a quick list element
-    var listNode = "<li>" + text + "</li>";
-
-    //Now use appendChild and add it to the list!
-    document.getElementById("listActual").appendChild(listNode);
-}
+inputButton.addEventListener('click', function() {
+    let newItem = document.createElement('li')
+    let removeListItem = () => {
+        newItem.remove()
+    }
+    newItem.innerHTML = listInput.value
+    listofToDoItems.appendChild(newItem)
+    listInput.value = ''
+    newItem.addEventListener('click', function(){
+        newItem.style.textDecoration = 'line-through'
+    })
+    newItem.addEventListener('click', function() {
+        setTimeout(removeListItem, 1000)
+    })
+})
